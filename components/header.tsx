004715c13a1a32b7559eb17df1e90d1dcd004e35
Toni Suominen <toni.suominen@devisioona.fi>
2021-12-21 22:27:57 +0200

    Page layout

---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-900 p-4">
+      <div className="text-white text-xl uppercase">Firehay</div>
+    </header>
+  );
+}
---
+++ pages/layout.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Header from "../components/header";
+import styles from "../styles/Home.module.css";
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Status - Firehay</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+}
