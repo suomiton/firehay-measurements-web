2ce6658afde1db07abf8ce6b3fc5639f4be56b92
Toni Suominen <toni@tonisuominen.com>
2022-05-19 22:23:32 +0300

    Temperature mappings and detailed cards

--- models/temperature.ts
+++ models/temperature.ts
@@ -1,26 +1,27 @@
-export interface FirehayStatus {
-    id: string;
-    locationId: string;
-    location: string;
-    temperature: number;
-    temp6Avg: number;
-    temp24Avg: number;
-    pressure: number;
-    humidity: number;
-    timeStamp: string;
+export interface FirehayStatusAM {
+    readonly id: string;
+    readonly locationId: string;
+    readonly location: string;
+    readonly temperature: number;
+    readonly temperature6Avg: number;
+    readonly temperature24Avg: number;
+    readonly temperatureRatio: number;
+    readonly pressure: number;
+    readonly humidity: number;
+    readonly timeStamp: string;
 };
 
-export interface FirehayLocationStatus extends FirehayStatus {
-    lastWeek: Measurement[];
+export interface FirehayLocationStatus extends FirehayStatusAM {
+    readonly lastWeek: MeasurementAM[];
 }
 
-export interface Measurement {
-    temperature: number;
-    timeStamp: string;
-    humidity: number;
-    pressure: number;
+export interface MeasurementAM {
+    readonly temperature: number;
+    readonly timeStamp: Date;
+    readonly humidity: number;
+    readonly pressure: number;
 }
 
-export interface ClientExtendedStatus extends FirehayStatus {
-    timeStampDistance: string;
+export interface ClientExtendedStatus extends FirehayStatusAM {
+    readonly timeStampDistance: string;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,43 +2,60 @@ import formatDistance from "date-fns/formatDistance";
 import type { InferGetStaticPropsType } from "next";
 import Link from "next/link";
 import { useMediaQuery } from "react-responsive";
-import { ClientExtendedStatus, FirehayStatus } from "../models/temperature";
+import { ClientExtendedStatus, FirehayStatusAM } from "../models/temperature";
 import { ListItemSlideIn, ListItemZoom } from "../utils/animations";
 import Api from "../utils/api";
 
-export const getStaticProps = async () => {
-  const statuses = await Api.get<FirehayStatus[]>("CurrentStatus");
-  const extendedStatus: ClientExtendedStatus[] = statuses.map((status) => ({
-    ...status,
-    timeStampDistance: formatDistance(new Date(status.timeStamp), new Date()),
-  }));
-
-  return {
-    props: {
-      statuses: extendedStatus,
-    },
-  };
-};
+type LinkItemProps = { isMobile: boolean; status: ClientExtendedStatus };
 
 function LinkItem({
-  status: { locationId, temperature, location, timeStampDistance },
-}: {
-  status: ClientExtendedStatus;
-}) {
+  status: {
+    locationId,
+    temperature,
+    location,
+    timeStampDistance,
+    temperatureRatio,
+    pressure,
+    humidity,
+  },
+  isMobile,
+}: LinkItemProps) {
   return (
     <Link href={`/location/${locationId}`}>
-      <a className="grid grid-flow-row grid-cols-5 gap-8 bg-gradient-to-r md:bg-gradient-to-b from-blue-100 to-orange-100 py-2 md:p-4">
-        <div className="col-span-1 md:col-span-5 text-right self-center md:order-1">
-          <span className="font-bold text-4xl md:text-7xl">
-            {Math.floor(temperature)}&deg;C
-          </span>
+      <a className="grid grid-flow-row grid-cols-10 md:grid-cols-5 gap-8 bg-gradient-to-r md:bg-gradient-to-b from-gray-100 to-orange-100 p-2 md:p-4">
+        <div className="col-span-4 sm:col-span-2 md:col-span-5 flex flex-col text-right self-center md:order-1">
+          {isMobile ? (
+            <div className="font-bold text-4xl">
+              {Math.floor(temperature)} &deg;C
+            </div>
+          ) : (
+            <div className="flex flex-row">
+              <div className="flex-initial self-center text-xs font-light leading-tight text-left">
+                <div className="text-gray-700">Humidity</div>
+                <div className="font-medium">{humidity} %</div>
+                <div className="text-gray-700">Pressure</div>
+                <div className="font-medium">{pressure} hPa</div>
+              </div>
+              <div className="flex-1">
+                <div className="font-bold text-7xl self-center">
+                  {Math.floor(temperature)} &deg;C
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-        <div className="col-span-4 md:col-span-5 flex flex-col justify-start content-center">
+        <div className="col-span-6 sm:col-span-8 md:col-span-5 flex flex-col justify-start content-center">
           <div className="flex-auto">
             <span className="text-xl">{location}</span>
           </div>
+          {isMobile && (
+            <div className="flex-auto text-xs font-light">
+              <span>{humidity} %</span>
+              <span className="ml-4">{pressure} hpa</span>
+            </div>
+          )}
           <div className="flex-auto">
-            <span className="text-xs">{timeStampDistance}</span>
+            <span className="text-xs font-light">{timeStampDistance}</span>
           </div>
         </div>
       </a>
@@ -54,15 +71,15 @@ function StatusListItem({
   index: number;
 }) {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
-  const className = "mb-4 shadow-sm rounded-md overflow-hidden";
+  const className = "mb-4 shadow-lg rounded-md overflow-hidden border bg-gray-100";
 
   return isMobile ? (
     <ListItemSlideIn index={index} className={className}>
-      <LinkItem status={status} />
+      <LinkItem status={status} isMobile={isMobile} />
     </ListItemSlideIn>
   ) : (
     <ListItemZoom index={index} className={className}>
-      <LinkItem status={status} />
+      <LinkItem status={status} isMobile={isMobile} />
     </ListItemZoom>
   );
 }
@@ -80,3 +97,17 @@ function Home({ statuses }: InferGetStaticPropsType<typeof getStaticProps>) {
 }
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const statuses = await Api.get<FirehayStatusAM[]>("CurrentStatus");
+  const extendedStatus: ClientExtendedStatus[] = statuses.map((status) => ({
+    ...status,
+    timeStampDistance: formatDistance(new Date(status.timeStamp), new Date()),
+  }));
+
+  return {
+    props: {
+      statuses: extendedStatus,
+    },
+  };
+};
