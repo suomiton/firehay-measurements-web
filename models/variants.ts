3f9dd014dca5f73aed2b867c00bfe4f4181644f2
Toni Suominen <toni@tonisuominen.com>
2022-05-19 22:22:55 +0300

    Animation and style tweaks

--- models/variants.ts
+++ models/variants.ts
@@ -1,17 +1,21 @@
+import { Keyframes } from "framer-motion";
+import { TransformProperties } from "framer-motion/types/motion/types";
+import { Transition, TransitionDefinition, Variant } from "framer-motion/types/types";
+
 const delayedSlideIn = {
     visible: (idx: number) => ({
         x: ["-100%", "15%", "0%"],
         transition: {
-            delay: idx * 0.1
+            delay: idx * 0.2
         }
     })
 };
 
 const delayedZoomIn = {
     visible: (idx: number) => ({
-        scale: [0, 1.2, 1],
+        scale: [0, 1.05, 1],
         transition: {
-            delay: idx * 0.1
+            delay: idx * 0.2,
         }
     })
 };
