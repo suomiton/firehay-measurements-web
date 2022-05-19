fac77aaba84caa36c944bd54064bf2f30f72f91a
Toni Suominen <toni@tonisuominen.com>
2022-05-19 22:22:17 +0300

    Header tweaks

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-gray-900 p-4">
-      <div className="text-white text-xl uppercase">Firehay</div>
+      <div className="text-white text-xl uppercase text-center">Firehay Status</div>
     </header>
   );
 }
